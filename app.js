4d983f7892e17aa5a29a96a731113988d06c421e
pny <reyno511@umn.edu>
2016-02-27 23:14:52 -0600

    openshift optimize

--- app.js
+++ app.js
@@ -56,5 +56,9 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.listen(port, ip);
 
 module.exports = app;
